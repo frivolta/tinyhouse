44cebe14d5cb8e6a4278b3a2ee88668dfc7b2785
frivolta <rivoltafilippo@gmail.com>
2020-07-30 19:34:34 +0200

    First commit

---
+++ tinyhouse_v1/server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/", (_req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port);
+
+console.log(`[app]: http://localhost:${port}`);
